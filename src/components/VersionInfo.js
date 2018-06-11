a820c34f74de604eb139dd74ff29f5d4fd0166de
Brendan Ragan <lordmortis@gmail.com>
2018-06-11 09:19:15 +0800

    show version info in the app

--- src/components/VersionInfo.js
+++ src/components/VersionInfo.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
  
 import Package from '../../package.json';
 
-export default class VersionInfo extends Component {
-  render() {
-    return (
-      <div className="version-info">
-        Translation Tool Helper Version: {Package.version}
-      </div>
-    );
-  }
+export default function VersionInfo(props) {
+  return (
+    <div id="version-info">
+      Translation Tool Helper Version: {Package.version}
+    </div>
+  );
 }
 
